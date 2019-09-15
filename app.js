a991ea0a0a4d27891995b39b5eed606a088b645b
dlrltjq1004 <48882958+dlrltjq1004@users.noreply.github.com>
2019-09-16 03:05:52 +0900

    Create Routing

--- app.js
+++ app.js
@@ -3,17 +3,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () => 
-console.log(`Listening on: http://localhost:${PORT}`);
-
-
-
 const handleHome = (req, res) => res.send("Hello from my ass");
-
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
@@ -23,12 +16,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-const middleware = (req, res, next) => {
-    res.send('not happening');
-};
-
-app.get("/", middleware, handleHome);
+app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
