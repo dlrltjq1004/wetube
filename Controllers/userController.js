b8ae322af1637eed8b5f64b5d2c54eb90c28a46e
dlrltjq1004 <48882958+dlrltjq1004@users.noreply.github.com>
2019-09-17 06:06:53 +0900

    views layout commit

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,7 +1,7 @@
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
-export const logout = (req, res) => res.send("Logout");
-export const users = (req, res) => res.send("Users");
-export const userDetail = (req, res) => res.send("UserDetail");
-export const editProfile = (req, res) => res.send("EditProfile");
-export const changePassword = (req, res) => res.send("ChangePassword");
+export const join = (req, res) => res.reder("join");
+export const login = (req, res) => res.reder("login");
+export const logout = (req, res) => res.reder("logout");
+export const users = (req, res) => res.reder("users");
+export const userDetail = (req, res) => res.reder("userDetail");
+export const editProfile = (req, res) => res.reder("editProfile");
+export const changePassword = (req, res) => res.reder("changePassword");
