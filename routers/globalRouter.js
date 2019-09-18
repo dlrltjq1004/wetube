11b8d899cbe8f47fdd0b42f00ab14876fcc7566c
dlrltjq1004 <48882958+dlrltjq1004@users.noreply.github.com>
2019-09-18 18:26:19 +0900

    Join Controller

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,13 +1,20 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../Controllers/videoController";
-import { join, login, logout } from "../Controllers/userController";
+import {
+  getJoin,
+  login,
+  logout,
+  postJoin
+} from "../Controllers/userController";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, home);
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.join, join);
+globalRouter.get(routes.home, home);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
