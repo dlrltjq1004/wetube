f18df81bec035b1237705fb00595f7a00ebfa33b
dlrltjq1004 <48882958+dlrltjq1004@users.noreply.github.com>
2019-09-21 21:49:21 +0900

    Create scss - Header, Footer, Login, Join, Home

--- webpack.config.js
+++ webpack.config.js
@@ -7,25 +7,36 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
       {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
         test: /\.(scss)$/,
         use: ExtractCSS.extract([
           {
+            // 이걸 사용해야  webpack 이 css를 이해한다.
             loader: "css-loader"
           },
           {
+            // css의 호환성과 관련된걸 해결해줌 EX) IE 지원문법변환
             loader: "postcss-loader",
             options: {
-              plugin() {
+              plugins() {
                 return [autoprefixer({ browsers: "cover 99.5%" })];
               }
             }
           },
           {
+            // scss or sass 를 일반 css로 변환해주는 plugin
             loader: "sass-loader"
           }
         ])
@@ -34,8 +45,9 @@ const config = {
   },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]"
+    filename: "[name].js"
   },
+  // user: ExtractCSS.extract 로 extract에 저장한 파일을 export 해준다
   plugins: [new ExtractCSS("styles.css")]
 };
 
