40ddf2e1592731c6e88eda68b6e05525fea082b5
dlrltjq1004 <48882958+dlrltjq1004@users.noreply.github.com>
2019-10-01 08:55:14 +0900

    facebook Login Callback

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -3,16 +3,25 @@ import routes from "../routes";
 import User from "../models/User";
 
 export const getJoin = (req, res) => {
-  res.render("join", { pageTitle: "Join" });
+  res.render("join", {
+    pageTitle: "Join"
+  });
 };
 
 export const postJoin = async (req, res, next) => {
   const {
-    body: { name, email, password, password2 }
+    body: {
+      name,
+      email,
+      password,
+      password2
+    }
   } = req;
   if (password !== password2) {
     res.status(400);
-    res.render("join", { pageTitle: "Join" });
+    res.render("join", {
+      pageTitle: "Join"
+    });
   } else {
     try {
       const user = await User({
@@ -29,7 +38,9 @@ export const postJoin = async (req, res, next) => {
 };
 
 export const getLogin = (req, res) =>
-  res.render("login", { pageTitle: "Log In" });
+  res.render("login", {
+    pageTitle: "Log In"
+  });
 
 export const postLogin = passport.authenticate("local", {
   failureRedirect: routes.login,
@@ -40,10 +51,17 @@ export const githubLogin = passport.authenticate("github");
 
 export const githubLoginCallback = async (_, __, profile, cb) => {
   const {
-    _json: { id, avatar_url: avatarUrl, name, email }
+    _json: {
+      id,
+      avatar_url: avatarUrl,
+      name,
+      email
+    }
   } = profile;
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({
+      email
+    });
     if (user) {
       user.githubId = id;
       user.save();
@@ -65,18 +83,55 @@ export const postGithubLogIn = (req, res) => {
   res.redirect(routes.home);
 };
 
+export const facebookLogin = passport.authenticate("facebook");
+
+export const facebookLoginCallback = (
+  accessToken,
+  refreshToken,
+  profile,
+  cb
+) => {
+  console.log(accessToken, refreshToken, profile, cb);
+};
+
+export const postFacebookLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
 export const logout = (req, res) => {
   req.logout();
   res.redirect(routes.home);
 };
 
 export const getMe = (req, res) => {
-  res.render("userDetail", { pageTitle: "User Detail", user: req.user });
+  res.render("userDetail", {
+    pageTitle: "User Detail",
+    user: req.user
+  });
 };
 
-export const userDetail = (req, res) =>
-  res.render("userDetail", { pageTitle: "User Detail" });
+export const userDetail = async (req, res) => {
+  const {
+    params: {
+      id
+    }
+  } = req;
+  try {
+    const user = await User.findById(id);
+
+    res.render("userDetail", {
+      pageTitle: "User Detail",
+      user
+    });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
 export const editProfile = (req, res) =>
-  res.render("editProfile", { pageTitle: "Edit Profile" });
+  res.render("editProfile", {
+    pageTitle: "Edit Profile"
+  });
 export const changePassword = (req, res) =>
-  res.render("changePassword", { pageTitle: "Change Password" });
+  res.render("changePassword", {
+    pageTitle: "Change Password"
+  });
