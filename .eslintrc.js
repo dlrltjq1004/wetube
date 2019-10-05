201b28ab8e57ed8f2e772a551b0106ab380a0b76
dlrltjq1004 <48882958+dlrltjq1004@users.noreply.github.com>
2019-10-05 12:10:18 +0900

    Mute Unmute

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,15 @@ module.exports = {
     es6: true
   },
   extends: ["airbnb-base", "plugin:prettier/recommended"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module"
-  },
+  // globals: {
+  //   Atomics: "readonly",
+  //   SharedArrayBuffer: "readonly"
+  // },
+  // parserOptions: {
+  //   ecmaVersion: 2018,
+  //   sourceType: "module"
+  // },
   rules: {
     "no-console": "off"
-  }
+  },
 };
